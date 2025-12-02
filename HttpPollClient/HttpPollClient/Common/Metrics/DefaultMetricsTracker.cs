197638a0fa199ca2fa42251bbd2226ad9a56dab2
veljkoselakovic <bg.selakovicveljko@gmail.com>
2025-12-02 14:30:29 -0800

    Creating MetricsTracker

---
+++ HttpPollClient/HttpPollClient/Common/Metrics/DefaultMetricsTracker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpPollClient.Common.Metrics
+{
+    public class DefaultMetricsTracker : IMetricsTracker
+    {
+        public DefaultMetricsTracker() { }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"[Metrics] {message}");
+        }
+    }
+}
---
+++ HttpPollClient/HttpPollClient/Common/Metrics/IMetricsTracker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpPollClient.Common.Metrics
+{
+    public interface IMetricsTracker
+    {
+        public void Log(string message);
+
+    }
+}
